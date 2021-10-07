9ce524f4894fc5e3e7060afcab8235398bcfc0ef
CoderNooob <87470723+CoderNooob@users.noreply.github.com>
2021-10-07 18:29:26 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+var noseX = 0
+var noseY = 0
+function preload()
+{
+   clown_nose = loadImage('https://i.postimg.cc/0jwbBxxj/mustache-header2.jpg')
+}
+
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center()
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized ლ(◉‿◉ ლ)')
+}
+
+function take_snapshot()
+{
+    save('virus.exe.png')
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY)
+    }
+}
+function draw()
+{
+     image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX -50, noseY -05, 110, 110);
+}
